2c151d79b38b82800fb13526d22b53676b76ab04
Seungwon-Choi <bestwondev@gmail.com>
2025-03-31 11:11:46 +0900

    initial commit

---
+++ src/main/java/com/backend/week1/BackendStudyWeek1Application.java
@@ -0,0 +1,13 @@
+package com.backend.week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudyWeek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStudyWeek1Application.class, args);
+    }
+
+}
